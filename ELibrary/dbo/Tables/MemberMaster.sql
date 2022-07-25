1c3d468743ee4cceac6077e1b0365a2f33dee439
Prutha Desai <prutha@cpssurat.com>
2022-07-25 15:09:47 +0530

    Database Table Creation added with SQL database project

---
+++ ELibrary/dbo/Tables/MemberMaster.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[MemberMaster] (
+    [MemberId]      INT           IDENTITY (1, 1) NOT NULL,
+    [FullName]      VARCHAR (200) NOT NULL,
+    [DOB]           DATE          NULL,
+    [ContactNumber] NVARCHAR (50) NULL,
+    [Email]         NVARCHAR (50) NULL,
+    [State]         VARCHAR (50)  NULL,
+    [City]          VARCHAR (50)  NULL,
+    [Pincode]       INT           NULL,
+    [Address]       VARCHAR (500) NULL,
+    [Password]      NVARCHAR (50) NOT NULL,
+    [AccountStatus] VARCHAR (50)  NULL,
+    CONSTRAINT [PK_MemberMaster] PRIMARY KEY CLUSTERED ([MemberId] ASC)
+);
+
